737542e8407808608b0e707e6306f166dc4fce22
vinh6789 <92259924+vinh6789@users.noreply.github.com>
2021-10-10 22:50:01 +0700

    first commit

---
+++ DAL_KhachSan/Dataprovider.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace DAL_KhachSan
+{
+    public class Dataprovider
+    {
+        private static Dataprovider instance;
+
+        public static Dataprovider Instance
+        {
+            get { if (instance == null) instance = new Dataprovider(); return Dataprovider.instance; }
+            set { Dataprovider.instance = value; }
+        }
+        private Dataprovider() { }
+        // sửa cái dòng này theo SQL bên máy chị
+        public string connectionSTR = @"Data Source=DESKTOP-KTOVLDC;Initial Catalog=QLKhachSan;Integrated Security=True";
+        public DataTable ExcuteQuery(string query, object[] parameter = null)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                if (parameter != null)
+                {
+                    string[] lispara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in lispara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(data);
+                connection.Close();
+            }
+            return data;
+
+
+        }
+        public int ExcuteNonQuery(string query, object[] parameter = null)
+        {
+            int data = 0;
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                if (parameter != null)
+                {
+                    string[] lispara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in lispara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                data = command.ExecuteNonQuery();
+                connection.Close();
+            }
+            return data;
+        }
+        public object ExcuteScalar(string query, object[] parameter = null)
+        {
+            object data = 0;
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                if (parameter != null)
+                {
+                    string[] lispara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in lispara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                data = command.ExecuteScalar();
+                connection.Close();
+            }
+            return data;
+
+
+        }
+    }
+}
--- QLKhachSan/Program.cs
+++ QLKhachSan/Program.cs
@@ -22,7 +22,7 @@ namespace QLKhachSan
             BonusSkins.Register();
             SkinManager.EnableFormSkins();
             UserLookAndFeel.Default.SetSkinStyle("DevExpress Style");
-            Application.Run(new frmDatPhong());
+            Application.Run(new Table_Phong());
         }
     }
 }
